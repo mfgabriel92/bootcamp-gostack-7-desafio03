f3044774bdad9845257f1145334b55e37b80fba7
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-08-17 10:22:11 -0300

    Refactor show specific meetup

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,4 +1,5 @@
-import { isBefore } from 'date-fns'
+import { parseISO, startOfDay, endOfDay, isBefore } from 'date-fns'
+import { Op } from 'sequelize'
 import Meetup from '../models/Meetup'
 import User from '../models/User'
 import File from '../models/File'
@@ -12,7 +13,7 @@ class MeetupController {
    * @param {Request} req
    * @param {Response} res
    */
-  async index(req, res) {
+  async show(req, res) {
     const { id } = req.params
 
     const meetup = await Meetup.findByPk(id, {
@@ -29,6 +30,40 @@ class MeetupController {
     })
 
     return res.json({ meetup })
+  }
+
+  /**
+   * Show a specific meetup
+   *
+   * @param {Request} req
+   * @param {Response} res
+   */
+  async index(req, res) {
+    const { date, page = 1 } = req.query
+    const parsedDate = parseISO(date)
+
+    const meetups = await Meetup.findAll({
+      where: {
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      include: [
+        {
+          model: File,
+          as: 'banner',
+        },
+        {
+          model: User,
+          as: 'user',
+        },
+      ],
+      limit: 10,
+      offset: (page - 1) * 10,
+      order: [['date', 'ASC']],
+    })
+
+    return res.json({ meetups })
   }
 
   /**
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,6 @@ import SessionController from './app/controllers/SessionController'
 import UserController from './app/controllers/UserController'
 import FileController from './app/controllers/FileController'
 import MeetupController from './app/controllers/MeetupController'
-import AllMeetupController from './app/controllers/AllMeetupController'
 import AttendantController from './app/controllers/AttendantController'
 import UserMeetupController from './app/controllers/UserMeetupController'
 import validSessionStore from './app/validators/SessionStore'
@@ -29,11 +28,11 @@ routes.use(auth)
 routes.put('/api/users', validUserUpdate, UserController.update)
 routes.get('/api/users/meetups', UserMeetupController.index)
 
-routes.get('/api/meetups', AllMeetupController.index)
+routes.get('/api/meetups', MeetupController.index)
 routes.get('/api/meetups/attending', AttendantController.index)
 routes.post('/api/meetups', validMeetupStore, MeetupController.store)
 routes.put('/api/meetups/:id', validMeetupUpdate, MeetupController.update)
-routes.get('/api/meetups/:id', MeetupController.index)
+routes.get('/api/meetups/:id', MeetupController.show)
 routes.delete('/api/meetups/:id', MeetupController.delete)
 routes.put('/api/meetups/:id/banner', upload, FileController.store)
 routes.post('/api/meetups/:id/attend', AttendantController.store)
